48798b4c3cd541841c9525e7777d156d45611ee0
casagrande7 <joaofootz10@gmail.com>
2024-03-19 09:50:24 -0300

    Atualizado

--- src/FlatListExample.tsx
+++ src/FlatListExample.tsx
@@ -1,6 +1,21 @@
 import React from "react";
 import { View } from "react-native";
 
+interface Item {
+    id: string;
+    nome: string;
+    idade: string;
+    email: string;
+}
+
+const dados: Item[] = [
+    {id: '1', nome: 'Aparecida' , idade: '77' , email: 'aparecida@gmail.com'},
+    {id: '2', nome: 'Maria' , idade: '22' , email: 'maria@gmail.com'},
+    {id: '3', nome: 'João' , idade: '90' , email: 'joao@gmail.com'},
+
+];
+
+
 function FlatListExample(): React.JSX.Element {
     return (
         <View>
