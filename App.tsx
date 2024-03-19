f48a3b455bd0f78922fd6b2ea53dda64b570bf52
casagrande7 <joaofootz10@gmail.com>
2024-03-19 09:44:03 -0300

    Atualizado

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ScrollViewExample from "./src/ScrollViewExample";
+import FlatListExample from "./src/FlatListExample";
 
 function App(): React.JSX.Element{
 
   return (
-    <ScrollViewExample>
-      
-    </ScrollViewExample>
+    <FlatListExample/>
   );
 }
 
---
+++ src/FlatListExample.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View } from "react-native";
+
+function FlatListExample(): React.JSX.Element {
+    return (
+        <View>
+
+        </View>
+    );
+}
+
+export default FlatListExample;
